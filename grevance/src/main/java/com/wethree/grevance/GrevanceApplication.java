85ba05b5657f4492c3414d429636eea569b5c037
Ritin <55983633+Ritin4@users.noreply.github.com>
2021-06-07 02:58:11 +0530

    Add files via upload

---
+++ grevance/src/main/java/com/wethree/grevance/GrevanceApplication.java
@@ -0,0 +1,13 @@
+package com.wethree.grevance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrevanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrevanceApplication.class, args);
+	}
+
+}
---
+++ grevance/src/test/java/com/wethree/grevance/GrevanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wethree.grevance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrevanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
